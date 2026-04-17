1be301e0407edca383954109d381510a92bdf033
rugtss <N00547108@upn.pe>
2026-04-17 10:35:52 -0500

    avance phyton

---
+++ S3-P/ejer3.py
@@ -0,0 +1,6 @@
+num1 = int(input("Ingrese número 1: "));
+num2 = int(input("ingrese numero 2: "));
+print(f"\nsSuma: {num1+num2}");
+print(f"Resta: {num1-num2}");
+print(f"Mutiplicacion: {num1*num2}")
+print(f"Division: {num1//num2}");
