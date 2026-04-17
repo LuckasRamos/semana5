7c9a8301634dc7fc1412c460edf175b57e74d3c8
rugtss <N00547108@upn.pe>
2026-04-17 10:17:01 -0500

    subiendo ejericios

---
+++ S3-P/ejer1.py
@@ -0,0 +1,7 @@
+n = input("ingrese su nombre: ")
+c = input("Ingrese si carrera: ")
+print("\n---impresion con +-----");
+print(n+", Bienvenido al curso FA de la carrera" + c);
+
+print("\n-----Imresion con $------");
+print(f"{n}, Bienvenido al curso Fa de la carrera  {c} ");
