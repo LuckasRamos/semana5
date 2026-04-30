fcb128824b99877ce99d51b4433626323606d830
Vortex2017-php <Nexark0212@gmail.com>
2026-04-30 15:59:07 -0500

    Enviando ejercicios

---
+++ SE4-C/ejer3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE4_C
+{
+    internal class ejer3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------SISTEMA DE CONVERSIONES DE MONEDA-----\n");
+            double soles = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n********* MENÚ DE OPCIONES *****");
+            Console.WriteLine("* 1.  Dolares  *");
+            Console.WriteLine("* 2.  Euros    *");
+            Console.WriteLine("*********************\n");
+
+            Console.Write("Ingrese una opción:");
+            int opc = int.Parse(Console.ReadLine());
+
+            switch (opc)
+            {
+                case 0: break;
+                    double dolares;
+                    dolares = soles / 3.75;
+
+                    Console.WriteLine("\nDolares: " + Math.Round(dolares));
+
+                    break;
+                case 1: break;
+                    double euro;
+                    euro = soles / 3.75;
+
+                    Console.WriteLine("\nDolares: " + Math.Round(euro,1));
+                    break;
+                default: Console.Write("\nOpción incorrecta"); break;
+            }
+
+        }
+    }
+}
